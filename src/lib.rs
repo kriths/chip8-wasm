3baea54a77f292384efbafb2d67f3a2d27aa289b
Thomas Krisch <thomas.krisch+git@gmail.com>
2020-12-12 13:42:01 +0100

    Add test and CI

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
-use wasm_bindgen::__rt::std::fs::read;
 use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn add(num1: i32, num2: i32) -> i32 {
     num1 + num2
+}
+
+#[cfg(test)]
+mod tests {
+    use super::add;
+
+    #[test]
+    fn it_works() {
+        assert_eq!(add(2, 2), 4);
+    }
 }
